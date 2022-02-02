bf3ebd4b64e1ae98511b23f9599e18bdc4392d51
Okiki <okikio.dev@gmail.com>
2022-02-02 05:43:52 +0000

    make renderer-mdx a workspace package

--- astro.config.ts
+++ astro.config.ts
@@ -9,13 +9,5 @@
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
   // Comment out "renderers: []" to enable Astro's default component support.
-  renderers: ['./renderer-mdx'],
-  // vite: {
-  //   plugins: [],
-  //   resolve: {
-  //     alias: {
-  //       'react/jsx-runtime': 'react/jsx-runtime.js',
-  //     },
-  //   },
-  // },
+  renderers: ['@astrojs/renderer-mdx'],
 });
